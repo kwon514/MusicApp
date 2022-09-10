f19c6f71d3df15e24a83cdc13457df6b30762495
kxhw <kevinxhwong@gmail.com>
2022-09-10 18:16:38 +1200

    completed backend repository and database

--- music/__init__.py
+++ music/__init__.py
@@ -4,22 +4,7 @@ from pathlib import Path
 from flask import Flask, render_template
 
 import music.adapters.repository as repo
-from music.adapters.memory_repository import MemoryRepository
-
-
-# TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-from music.domainmodel.track import Track
-
-
-# TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-def create_some_track():
-    some_track = Track(1, "Heat Waves")
-    some_track.track_duration = 250
-    some_track.track_url = 'https://spotify/track/1'
-    return some_track
-
+from music.adapters.memory_repository import MemoryRepository, populate
 
 def create_app(test_config=None):
     # Create the Flask app object.
@@ -27,7 +12,7 @@ def create_app(test_config=None):
 
     # Configure the app from configuration-file settings.
     app.config.from_object('config.Config')
-    data_path = Path('covid') / 'adapters' / 'data'
+    data_path = Path('music') / 'adapters' / 'data'
 
     if test_config is not None:
         # Load test configuration, and override any configuration settings.
@@ -36,6 +21,9 @@ def create_app(test_config=None):
 
     # Create the MemoryRepository implementation for a memory-based repository.
     repo.repo_instance = MemoryRepository()
+
+    # Fill the content of the repository from the CSV files.
+    populate(data_path, repo.repo_instance)
 
     with app.app_context():
         # Register blueprints.
--- music/adapters/memory_repository.py
+++ music/adapters/memory_repository.py
@@ -1,5 +1,9 @@
 from typing import List
+import csv
+from pathlib import Path
+from music.adapters import csvdatareader
 
+from music.adapters.csvdatareader import TrackCSVReader
 from music.adapters.repository import AbstractRepository, RepositoryException
 from music.domainmodel.user import User
 from music.domainmodel.track import Track
@@ -9,8 +13,9 @@ from music.domainmodel.genre import Genre
 from music.domainmodel.review import Review
 from music.domainmodel.playlist import PlayList
 
+
 class MemoryRepository(AbstractRepository):
-    
+
     def __init__(self):
         self.__users = list()
         self.__tracks = list()
@@ -19,18 +24,39 @@ class MemoryRepository(AbstractRepository):
         self.__genres = list()
         self.__reviews = list()
         self.__playlists = list()
-        
+
     def add_user(self, user: User):
         self.__users.append(user)
-    
+
     def get_user(self, user_name) -> User:
         return next((user for user in self.__users if user.user_name == user_name), None)
-    
+
+    def add_track_list(self, track_list: list):
+        self.__tracks = track_list
+
+    def add_album_list(self, album_list: list):
+        self.__albums = album_list
+
+    def add_artist_list(self, artist_list: list):
+        self.__artists = artist_list
+
+    def add_genre_list(self, genre_list: list):
+        self.__genres = genre_list
+
     def get_tracks_by_title(self, track_title: str) -> List[Track]:
         return [track for track in self.__tracks if track.title.lower() == track_title.strip().lower()]
-    
+
     def get_tracks_by_artist(self, track_artist: str) -> List[Track]:
         return [track for track in self.__tracks if track.artist.full_name.lower() == track_artist.strip().lower()]
-    
+
     def get_tracks_by_album(self, album_name: str) -> List[Track]:
         return [track for track in self.__tracks if track.album.title.lower() == album_name.strip().lower()]
+
+
+def populate(data_path: Path, repo: MemoryRepository):
+    TrackCSVReader(str(Path(data_path) / 'raw_albums_excerpt.csv'),
+                   str(Path(data_path) / 'raw_tracks_excerpt.csv')).read_csv_files()
+    repo.add_track_list(TrackCSVReader.dataset_of_tracks)
+    repo.add_artist_list(TrackCSVReader.dataset_of_artists)
+    repo.add_album_list(TrackCSVReader.dataset_of_albums)
+    repo.add_genre_list(TrackCSVReader.dataset_of_genres)
