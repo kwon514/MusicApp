390507aeae2908aed6b4d8d602ff3a8b19a096d7
kxhw <kevinxhwong@gmail.com>
2022-09-11 06:33:42 +1200

    refined memory repository methods

--- music/adapters/memory_repository.py
+++ music/adapters/memory_repository.py
@@ -31,20 +31,20 @@ class MemoryRepository(AbstractRepository):
     def get_user(self, user_name) -> User:
         return next((user for user in self.__users if user.user_name == user_name), None)
 
-    def add_track_list(self, track_list: list):
-        self.__tracks = track_list
+    def set_track_list(self, track_list: list):
+        self.__tracks += track_list
 
-    def add_album_list(self, album_list: list):
-        self.__albums = album_list
+    def set_album_list(self, album_list: list):
+        self.__albums += album_list
 
-    def add_artist_list(self, artist_list: list):
-        self.__artists = artist_list
+    def set_artist_list(self, artist_list: list):
+        self.__artists += artist_list
 
-    def add_genre_list(self, genre_list: list):
-        self.__genres = genre_list
+    def set_genre_list(self, genre_list: list):
+        self.__genres += genre_list
 
     def get_tracks_by_title(self, track_title: str) -> List[Track]:
-        return [track for track in self.__tracks if track.title.lower() == track_title.strip().lower()]
+        return [track for track in self.__tracks if track.title.lower()[0:len(track_title)] == track_title.strip().lower()[0:len(track_title)]]
 
     def get_tracks_by_artist(self, track_artist: str) -> List[Track]:
         return [track for track in self.__tracks if track.artist.full_name.lower() == track_artist.strip().lower()]
@@ -54,9 +54,8 @@ class MemoryRepository(AbstractRepository):
 
 
 def populate(data_path: Path, repo: MemoryRepository):
-    TrackCSVReader(str(Path(data_path) / 'raw_albums_excerpt.csv'),
-                   str(Path(data_path) / 'raw_tracks_excerpt.csv')).read_csv_files()
-    repo.add_track_list(TrackCSVReader.dataset_of_tracks)
-    repo.add_artist_list(TrackCSVReader.dataset_of_artists)
-    repo.add_album_list(TrackCSVReader.dataset_of_albums)
-    repo.add_genre_list(TrackCSVReader.dataset_of_genres)
+    file_data = TrackCSVReader(str(Path(data_path) / 'raw_albums_excerpt.csv'), str(Path(data_path) / 'raw_tracks_excerpt.csv'))
+    file_data.read_csv_files()
+    repo.set_track_list(file_data.dataset_of_tracks)
+    repo.set_album_list(file_data.dataset_of_albums)
+    repo.set_genre_list(file_data.dataset_of_genres)
