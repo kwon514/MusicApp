d0ba36120ad77da6b475e816e0f75df198bfce54
kxhw <kevinxhwong@gmail.com>
2022-09-11 19:32:51 +1200

    refined search input styling

--- music/search/search.py
+++ music/search/search.py
@@ -7,7 +7,6 @@ search_blueprint = Blueprint(
     'search_bp', __name__)
 
 
-
 @search_blueprint.route('/search', methods=['GET'])
 def search():
     return render_template('search/search.html')
@@ -16,15 +15,11 @@ def search():
 @search_blueprint.route('/search_tracks', methods=['GET'])
 def search_tracks():
     target_track = request.args.get('target_track')
-    if target_track is "":
+    if target_track == "":
         return redirect(url_for('search_bp.search'))
-    
+
     matching_tracks = services.get_tracks(target_track, repo.repo_instance)
-    for track in matching_tracks:
-        print(track.title)
-        print(track.artist.full_name)
-        print(track.album.title)
-    
+
     if len(matching_tracks) > 0:
         return render_template('search/search_tracks.html', matching_tracks=matching_tracks)
     return render_template('search/search.html')
