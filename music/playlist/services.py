fab55ac3a3e131d23cac73c3ac42b7089f2a1bc9
kxhw <kevinxhwong@gmail.com>
2022-09-22 17:04:03 +1200

    added playlist copy and sharing feature

--- music/playlist/services.py
+++ music/playlist/services.py
@@ -9,6 +9,7 @@ from music.domainmodel.genre import Genre
 from music.domainmodel.review import Review
 from music.domainmodel.playlist import PlayList
 
+
 class PlaylistNotUniqueException(Exception):
     pass
 
@@ -16,19 +17,22 @@ class PlaylistNotUniqueException(Exception):
 class UnknownUserException(Exception):
     pass
 
+
 def get_user(user_name: str, repo: AbstractRepository):
     user = repo.get_user(user_name)
     if user is None:
         raise UnknownUserException
-    return user    
+    return user
 
-def get_playlists(user_name: str,repo: AbstractRepository):
+
+def get_playlists(user_name: str, repo: AbstractRepository):
     user = repo.get_user(user_name)
     if user is None:
         raise UnknownUserException
     return repo.get_playlists(user)
 
-def add_playlist(playlist_name: str, user_name: str, repo: AbstractRepository):    
+
+def add_playlist(playlist_name: str, user_name: str, repo: AbstractRepository):
     playlist = repo.get_playlist(playlist_name)
     if playlist is not None:
         raise PlaylistNotUniqueException
@@ -38,6 +42,7 @@ def add_playlist(playlist_name: str, user_name: str, repo: AbstractRepository):
     playlist = PlayList(playlist_name, user)
     repo.add_playlist(playlist)
 
+
 def get_playlist(playlist_name: str, user_name: str, repo: AbstractRepository):
     user = repo.get_user(user_name)
     if user is None:
@@ -45,16 +50,31 @@ def get_playlist(playlist_name: str, user_name: str, repo: AbstractRepository):
     playlist = repo.get_playlist(playlist_name)
     return playlist
 
+
 def get_playlist_without_username(playlist_name: str, repo: AbstractRepository):
     playlist = repo.get_playlist(playlist_name)
-    return playlist    
+    return playlist
+
 
 def add_track(track: Track, playlist_name: str, repo: AbstractRepository):
     repo.add_track(track, playlist_name)
 
+
 def get_track_by_id(track_id: int, repo: AbstractRepository):
-    return repo.get_track_by_id(track_id)    
+    return repo.get_track_by_id(track_id)
+
 
 def get_list_of_tracks(playlist_name: str, repo: AbstractRepository):
-    return repo.get_list_of_tracks(playlist_name)    
-  
+    return repo.get_list_of_tracks(playlist_name)
+
+
+def create_playlist_from_list(playlist_name: str, list_of_tracks: List[Track], user_name: str, repo: AbstractRepository):
+    playlist_copy_name = "Copy of " + playlist_name
+    playlist = repo.get_playlist(playlist_copy_name)
+    if playlist is not None:
+        raise PlaylistNotUniqueException
+    user = repo.get_user(user_name)
+    playlist = PlayList(playlist_copy_name, user)
+    repo.add_playlist(playlist)
+    for track in list_of_tracks:
+        repo.add_track(track, playlist_copy_name)
